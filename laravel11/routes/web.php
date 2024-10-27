95843857c376c29f95e1088d4c5a833583b7d1e7
Aco Koca <haidar.223040093@mail.unpas.ac.id>
2024-10-27 12:52:09 +0700

    Tugas 5: View Data Laravel

--- laravel11/routes/web.php
+++ laravel11/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,48 @@ Route::get('/about', function () {
     return view('about', array('nama' => 'Haidar Ali', 'title' => 'About Page'));
 });
 
-Route::get('/blog', function () {
-    return view('blog', array('title' => 'Blog Page'));
+Route::get('/posts', function () {
+	return view('posts', array('title' => 'Blog Page', 'posts' => array(
+		array(
+			'id' => 1,
+			'slug' => 'judul-artikel-1',
+			'title' => 'Judul Artikel 1',
+			'author' => 'Haidar Ali',
+			'body' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Adipiscing suscipit torquent erat semper maecenas urna ex lectus. Nec montes cras primis tincidunt suspendisse ex sociosqu. Aliquam torquent elementum aenean senectus vulputate donec et urna blandit.'
+		),
+		array(
+			'id' => 2,
+			'slug' => 'judul-artikel-2',
+			'title' => 'Judul Artikel 2',
+			'author' => 'Haidar Ali',
+			'body' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl amet posuere scelerisque vulputate convallis elementum tempus tellus. Per fringilla quis aliquam montes quisque.'
+		)
+	)));
+});
+
+Route::get('/posts/{slug}', function($slug) {
+	$posts = array(
+		array(
+			'id' => 1,
+			'slug' => 'judul-artikel-1',
+			'title' => 'Judul Artikel 1',
+			'author' => 'Haidar Ali',
+			'body' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Adipiscing suscipit torquent erat semper maecenas urna ex lectus. Nec montes cras primis tincidunt suspendisse ex sociosqu. Aliquam torquent elementum aenean senectus vulputate donec et urna blandit.'
+		),
+		array(
+			'id' => 2,
+			'slug' => 'judul-artikel-2',
+			'title' => 'Judul Artikel 2',
+			'author' => 'Haidar Ali',
+			'body' => 'Lorem ipsum odor amet, consectetuer adipiscing elit. Nisl amet posuere scelerisque vulputate convallis elementum tempus tellus. Per fringilla quis aliquam montes quisque.'
+		)
+	);
+
+	$post = Arr::first($posts, function($post) use ($slug) {
+		return $post['slug'] == $slug;
+	});
+
+	return view('post', array('title' => 'Single Post Page', 'post' => $post));
 });
 
 Route::get('/contact', function () {
